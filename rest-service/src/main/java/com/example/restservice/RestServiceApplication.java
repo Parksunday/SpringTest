7c0784ade1d01481c7d5a64837b88bb61de6ef1e
Parksunday <athis_a@yahoo.com>
2020-02-16 20:48:35 +0700

    Test

---
+++ rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,22 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestServiceApplication.class, args);
+
+		//Create a Resource Representation Class
+
+//        The service will handle GET requests for /greeting,
+//        optionally with a name parameter in the query string.
+//        The GET request should return a 200 OK response with JSON in the body that represents a greeting.
+//
+
+	}
+
+}
